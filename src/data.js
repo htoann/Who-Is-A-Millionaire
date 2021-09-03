dd5c549329ef87463b3046da51a8864d83c0fb79
HToan <huutrantoan@gmail.com>
2021-09-03 23:50:46 +0700

    Updating question

--- src/data.js
+++ src/data.js
@@ -1,67 +1,246 @@
 export const data = [
   {
     id: 1,
-    question: "Rolex is a company that specializes in what type of product?",
+    question:
+      "Real Madrid  là một câu lạc bộ bóng đá chuyên nghiệp của Tây Ban Nha có trụ sở tại",
     answers: [
       {
-        text: "Phone",
-        correct: false,
-      },
-      {
-        text: "Watches",
+        text: "Marid",
         correct: true,
       },
       {
-        text: "Food",
+        text: "Marib",
         correct: false,
       },
       {
-        text: "Cosmetic",
+        text: "Argentina",
+        correct: false,
+      },
+      {
+        text: "Hà Nội",
         correct: false,
       },
     ],
   },
   {
     id: 2,
-    question: "When did the website `Facebook` launch?",
+    question: "2 họng súng xuất sắc nhất Real trong 10 năm qua là",
     answers: [
       {
-        text: "2004",
+        text: "Ronaldo và Benzema",
         correct: true,
       },
       {
-        text: "2005",
+        text: "Ronaldo và Figo",
         correct: false,
       },
       {
-        text: "2006",
+        text: "Ronaldo và Messi",
         correct: false,
       },
       {
-        text: "2007",
+        text: "Ronaldinho và Benzema",
         correct: false,
       },
     ],
   },
   {
     id: 3,
-    question: "Who played the character of harry potter in movie?",
+    question:
+      "CLB đã chi nhiều tiền nhất trong kì chuyển nhượng hè 2020 - 2021?",
     answers: [
       {
-        text: "Johnny Deep",
+        text: "PSG",
         correct: false,
       },
       {
-        text: "Leonardo Di Caprio",
-        correct: false,
-      },
-      {
-        text: "Denzel Washington",
-        correct: false,
-      },
-      {
-        text: "Daniel Red Cliff",
+        text: "Arsenal",
         correct: true,
+      },
+      {
+        text: "Manchester United",
+        correct: false,
+      },
+      {
+        text: "Manchester City",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question:
+      "Một trong những cầu thủ xuất sắc của châu Á đang thi đấu ở ngoại hạng anh?",
+    answers: [
+      {
+        text: "Đặng Văn Lâm",
+        correct: false,
+      },
+      {
+        text: "Son Heung-min",
+        correct: true,
+      },
+      {
+        text: "Harry Kane",
+        correct: false,
+      },
+      {
+        text: "Theo Hernandez",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Ai từng là đội trưởng của Real Marid?",
+    answers: [
+      {
+        text: "Sergio Ramos",
+        correct: true,
+      },
+      {
+        text: "Leo Messi",
+        correct: false,
+      },
+      {
+        text: "Kaká",
+        correct: false,
+      },
+      {
+        text: "Gareth Bale",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: 'Bộ phim "Chị Dậu" được chuyển thể từ tác phẩm văn học nào?',
+    answers: [
+      {
+        text: "Người mẹ cầm súng",
+        correct: false,
+      },
+      {
+        text: "Vợ chồng A Phủ",
+        correct: false,
+      },
+      {
+        text: "Tắt đèn",
+        correct: true,
+      },
+      {
+        text: "Tuổi thơ dữ dội",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "Haiku là thể thơ truyền thống của nước nào?",
+    answers: [
+      {
+        text: "Nhật Bản",
+        correct: true,
+      },
+      {
+        text: "Mông Cổ",
+        correct: false,
+      },
+      {
+        text: "Trung Quốc",
+        correct: false,
+      },
+      {
+        text: "Hàn Quốc",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "Bản hợp đồng chuyển nhượng bom tấn của Real Marid thuộc về?",
+    answers: [
+      {
+        text: "Gareth Bale",
+        correct: true,
+      },
+      {
+        text: "Cristiano Ronaldo",
+        correct: false,
+      },
+      {
+        text: "James Rodríguez",
+        correct: false,
+      },
+      {
+        text: "Zinedine Zidane",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "Who is the “Queen of Pop”",
+    answers: [
+      {
+        text: "Lourdes Leon",
+        correct: false,
+      },
+      {
+        text: "Lady Gaga",
+        correct: false,
+      },
+      {
+        text: "Cyndi Lauper",
+        correct: false,
+      },
+      {
+        text: "Madonna",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "EURO lần đầu tiên tổ chức tại quốc gia nào?",
+    answers: [
+      {
+        text: "Pháp",
+        correct: true,
+      },
+      {
+        text: "Ý",
+        correct: false,
+      },
+      {
+        text: "Đức",
+        correct: false,
+      },
+      {
+        text: "Anh",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "Tác giả “Bàn tay của Chúa” phiên bản người Pháp",
+    answers: [
+      {
+        text: "T. Henry",
+        correct: true,
+      },
+      {
+        text: "Maradona",
+        correct: false,
+      },
+      {
+        text: "Zinédine Zidane",
+        correct: false,
+      },
+      {
+        text: "Michel Platini",
+        correct: false,
       },
     ],
   },
